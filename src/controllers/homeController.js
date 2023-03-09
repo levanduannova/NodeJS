b2a004a0758c41e8bd72b347ea6f5bbdcb2b19c0
levanduandut <fbsinon300101@gmail.com>
2023-03-09 17:16:31 +0700

    connect db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,17 +1,24 @@
-
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
-}
+import db from "../models/index";
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    return res.render("homepage.ejs",{
+        data: JSON.stringify(data)
+    });
+  } catch(e) {
+    console.log(e)
+  }
+};
 
 let getAboutPage = (req, res) => {
-    return res.render('test/about.ejs');
-}
+  return res.render("test/about.ejs");
+};
 
 // // object: {
 // //     key: '',
 // //     value: ''
 // // }
 module.exports = {
-    getHomePage: getHomePage,
-    getAboutPage: getAboutPage
-}
+  getHomePage: getHomePage,
+  getAboutPage: getAboutPage,
+};
