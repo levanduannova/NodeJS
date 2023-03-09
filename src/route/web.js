272543b5bd7fa55c7f10072caadb1ea48f6184c5
levanduandut <fbsinon300101@gmail.com>
2023-03-09 16:34:58 +0700

    first commit

---
+++ src/route/web.js
@@ -0,0 +1,20 @@
+import express from "express";
+import homeController from "../controllers/homeController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+  router.get('/', homeController.getHomePage);
+  router.get('/about', homeController.getAboutPage);
+  
+//   router.get("/", (res, req) => {
+//     return req.send("Xin chào các bạn ");
+//   });
+  router.get("/duan", (res, req) => {
+    return req.send("Duan ");
+  });
+
+  return app.use("/", router);
+};
+
+module.exports = initWebRoutes;
